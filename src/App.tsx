d95758d41af665daf50f4d678e455b862a58d00c
ThanHtikeZaw <125514456+ThanHtikeZawGithub@users.noreply.github.com>
2023-09-06 15:55:03 +0630

    added slack web hook to test in production

--- src/App.tsx
+++ src/App.tsx
@@ -35,7 +35,7 @@ const App = () => {
     // Send data to Slack
     if (formData) {
       try {
-        await fetch("https://hooks.slack.com/services/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",
+        await fetch("https://hooks.slack.com/services/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",
           {
             method: "POST",
             mode: "no-cors",
